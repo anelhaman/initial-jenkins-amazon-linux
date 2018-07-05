7e6bf8bdf81ac903f2497205758a4d07a44c0fa7
EC2 Default User <ec2-user@ip-10-4-42-84.ap-southeast-1.compute.internal>
2018-07-05 09:09:50 +0000

    remove 2-xt

--- 2-xt-changeuser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo chown -R jenkins:jenkins /var/lib/jenkins 
-sudo chown -R jenkins:jenkins /var/cache/jenkins
-sudo chown -R jenkins:jenkins /var/log/jenkins
-export JENKINS_USER="jenkins"
-sudo service jenkins restart
