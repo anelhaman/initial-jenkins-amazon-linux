2d018b418a9089bd042c359da7fe598a13b837dd
EC2 Default User <ec2-user@ip-172-22-8-200.ap-southeast-1.compute.internal>
2018-12-13 05:24:09 +0000

    Add script remove php-fpm-7.0

---
+++ 5-remove-php70.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo yum update -y
+sudo yum -y remove php70 php70-mysqlnd php70-imap php70-pecl-memcache php70-pecl-apcu php70-gd
+sudo yum -y remove php70-pdo php70-opcache php70-xml php70-mcrypt php70-devel php70-intl php70-mbstring php70-bcmath php70-json php70-iconv php70-soap php70-zip
+sudo yum -y remove php70-fpm
